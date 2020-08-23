9d7693bf3cdf3851ed7983627fcf0d12aff00172
Vladislav Melnik <vladislav.melnik@protonmail.com>
2020-08-23 07:54:15 +0300

    move to new version

--- vru-transport/src/protocol/sphinx.rs
+++ vru-transport/src/protocol/sphinx.rs
@@ -1,36 +1,14 @@
-use vru_sphinx::{PseudoRandomStream, AuthenticatedMessage};
+use vru_sphinx::AuthenticatedMessage;
 use secp256k1::SecretKey;
 use sha2::Sha256;
-use chacha::{ChaCha, Error as ChaChaError, SeekableKeyStream, KeyStream};
+use chacha20::ChaCha20;
 use hmac::Hmac;
 use generic_array::{
     GenericArray,
-    typenum::{U4, U19, U32, U181, U233},
+    typenum::{U4, U19, U181, U233},
 };
 
-pub struct PseudoRandomStreamChaCha32(ChaCha);
-
-impl PseudoRandomStream<U32> for PseudoRandomStreamChaCha32 {
-    fn seed(v: GenericArray<u8, U32>) -> Self {
-        let mut array = [0; 32];
-        array.copy_from_slice(v.as_ref());
-        PseudoRandomStreamChaCha32(ChaCha::new_chacha20(&array, &[0u8; 8]))
-    }
-}
-
-impl KeyStream for PseudoRandomStreamChaCha32 {
-    fn xor_read(&mut self, dst: &mut [u8]) -> Result<(), ChaChaError> {
-        self.0.xor_read(dst)
-    }
-}
-
-impl SeekableKeyStream for PseudoRandomStreamChaCha32 {
-    fn seek_to(&mut self, byte_offset: u64) -> Result<(), ChaChaError> {
-        self.0.seek_to(byte_offset)
-    }
-}
-
-pub type Sphinx = (SecretKey, Hmac<Sha256>, Sha256, PseudoRandomStreamChaCha32);
+pub type Sphinx = (SecretKey, Hmac<Sha256>, Sha256, ChaCha20);
 
 /// size = 33 + (19 + 32) * 4 + 32 + 115 = 384 <= 0x1fc
 pub type OutgoingInitialPacket = AuthenticatedMessage<Sphinx, U19, U4, GenericArray<u8, U181>>;
